f62d9eb9428b763f1fcfe08bb187173b8b8f6400
Győző Dobner <46606056+gyoezoedobnerjr@users.noreply.github.com>
2021-10-24 16:23:45 +0200

    Update site.pp
    Testing the 'group' and 'user' resource declarations.

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,2 +1,16 @@
 node default {
+  group {'kacabajka':
+    ensure => 'present',
+    allowdupe => 'false'    
+  }
+  user {'tapsihapsi':
+    ensure => 'present',
+    allowdupe => 'false'
+  }
+  file {'/root/humbug'
+    ensure => 'file',
+    content => 'Just some text.',
+    owner => 'tapsihapsi',
+    group => 'kacabajka'
+  }
 }
