46691525c2e319b8b9e68ef5b0a8be0b93e37879
Győző Dobner <46606056+gyoezoedobnerjr@users.noreply.github.com>
2021-10-24 17:32:27 +0200

    Update site.pp
    Testing the 'package' resource declaration with 'mc'.

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,16 +1,7 @@
 node default {
-  group {'kacabajka':
-    ensure => 'absent',
-    allowdupe => 'false'    
-  }
-  user {'tapsihapsi':
-    ensure => 'absent',
-    allowdupe => 'false'
-  }
-  file {'/root/humbug':
-    ensure => 'absent',
-    content => 'Just some text.',
-    owner => 'tapsihapsi',
-    group => 'kacabajka'
+  package {'mc':
+    name => 'Midnight Commander',
+    command => '/bin/yum',
+    ensure => 'installed'
   }
 }
