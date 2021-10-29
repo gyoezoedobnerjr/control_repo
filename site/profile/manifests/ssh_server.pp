03a1ddb317afd7b6bc0c42eab1c46193d1b97bab
Győző Dobner <solfegeing@gmail.com>
2021-10-29 13:17:36 +0200

    Adding public key to 'site/profile/manifests/ssh_server.pp' explicitly to try to make ssh setup work.

--- site/profile/manifests/ssh_server.pp
+++ site/profile/manifests/ssh_server.pp
@@ -10,6 +10,6 @@ class profile::ssh_server {
 		ensure => present,
 		user   => 'root',
 		type   => 'ssh-rsa',
-		key    => "$ssh.rsa.key",
+		key    => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQDfzjcQ3A7uYvGRqyYSgb0mkZfRd/igE0oMjnaD0xKrZltCWwCxuM2l01S7g25jzb8OQ6y9Trcmid72ZmEEogYeYVQ1ZbI0EKVv6ZtfSPKwhLJMjDzjjDrTcsebkLX7ppLyt4teZMf8IR22jADrRAX0w6xdR/TfQhi59dXlCfQfmBt770B6kwC0ePyY8/JAzaelujEi1q9klrlttJgy+A00nrxbGfrA0qG7wsrlXOpLW1KgqsT5vqlPaVqWx/IL3vveaqDu1A5y4RY3i10Aks/h9PgwNr8I3gISCUg1FxbuhOASZiPohIXAp4aXZF5eJ45chy7turK8fX9oKfayMqcz',
 	}  
 }
